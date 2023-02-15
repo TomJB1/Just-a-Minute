5af386e277a8e9c2ebfd53ada8f24f1f7ca7eba8
TomJB1 <tombrandisicloud@gmail.com>
2023-02-15 18:07:50 +0000

    Create files and added event listener for key

---
+++ script.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('keydown', function(event) {
+    key = event.key;
+    });
+
+
+
+function bindBuzzers(playerNumber)
+{
+    //for (let i = 0; i < playerNumber; i++) {
+        
+    //  }
+}
